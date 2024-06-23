98bab4753bbf1bb28827cb61b5732b48931e2137
Prashant Erappa Yelurkar <prashantyelurkar2004@gmail.com>
2024-06-23 12:35:32 +0530

    done

--- src/pages/generatePdf.tsx
+++ src/pages/generatePdf.tsx
@@ -1,24 +1,25 @@
-// import React from "react";
-// import dynamic from "next/dynamic";
+import React from "react";
+import dynamic from "next/dynamic";
 
-// // Dynamically import PDFDownloadLink with SSR disabled
-// const PDFDownloadLink = dynamic(
-//   () => import("@react-pdf/renderer").then((mod) => mod.PDFDownloadLink),
-//   { ssr: false }
-// );
+// Dynamically import PDFDownloadLink with SSR disabled
+const PDFDownloadLink = dynamic(
+  () => import("@react-pdf/renderer").then((mod) => mod.PDFDownloadLink),
+  { ssr: false }
+);
 // import InvoicePDF from "../Components/InvoicePDF";
 
-// const GeneratePDF = () => {
-//   return (
-//     <div>
-//       <h1>Generate Invoice PDF</h1>
-//       {PDFDownloadLink && (
-//         <PDFDownloadLink document={<InvoicePDF />} fileName="invoice.pdf">
-//           {({ loading }) => (loading ? "Loading document..." : "Download PDF")}
-//         </PDFDownloadLink>
-//       )}
-//     </div>
-//   );
-// };
+const GeneratePDF = () => {
+  return (
+    // <div>
+    //   <h1>Generate Invoice PDF</h1>
+    //   {PDFDownloadLink && (
+    //     <PDFDownloadLink document={<InvoicePDF />} fileName="invoice.pdf">
+    //       {({ loading }) => (loading ? "Loading document..." : "Download PDF")}
+    //     </PDFDownloadLink>
+    //   )}
+    // </div>
+    <h1>Hllo</h1>
+  );
+};
 
-// export default GeneratePDF;
+export default GeneratePDF;
